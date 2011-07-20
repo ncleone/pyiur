49a00027dea9fb075a0fb061728345b044e48bdb
Derek Nuño <derek.nuno@gmail.com>
2011-07-20 09:19:53 -0500

    Examples with object API

--- examples/download.py
+++ examples/download.py
@@ -6,49 +6,34 @@ imgur.com account.
 
 """
 
+from __future__ import with_statement
+
 import getpass
 
 import pyiur
 import requests
 
-def image_links(auth):
-    """Yields all original image links for the authenticated user."""
+if __name__ == '__main__':
+    # Authenticate with imgur.com using cookie authentication.
+    username = raw_input('imgur.com username: ')
+    password = getpass.getpass('imgur.com password: ')
+    imgur = pyiur.Imgur(auth = (username, password))
 
-    # We can't get all images at once, so we need to get each page of images.
-    num_pages = pyiur.get_images_pages(auth = auth)
+    # Get the number of images to show progess while downloading.
+    # Save it, because imgur.num_images represents the number of images on the
+    # server, which may change as we download images.
+    num_images = imgur.num_images
 
-    # Pages are indexed from one.
-    for page in xrange(1, num_pages + 1):
-        # Gets the images for the current page and yields the link to the
-        # original image back to the caller.
-        #
-        # Note: I like to put auth at the back, so I always pass it by name.
-        page_images = pyiur.get_images(page = page, auth = auth)
- 
-        for image in page_images:
-            yield image.links['original']
-
-def download_images(auth):
-    """Downloads and saves all of the images for the authenticated user."""
-
-    # Create an iterator that yields all of the image links.
-    links = image_links(auth = auth)
-
-    # Get the number of images to give a nicer downloading message.
-    count = pyiur.get_images_count(auth = auth)
-
-    # Let's loop over all of the links to the original images.
-    for i, url in enumerate(links, 1):
-        # Use the resource image name for our file.
-        filename = url.rsplit('/')[-1]
-
-        # Time to download the image.
-        print u'({1}/{2}) Downloading image {0}...'.format(filename, i, count),
-        response = requests.get(url)
+    # Loop over all of the images and count them.
+    for i, image in enumerate(imgur.images):
+        # Download the image.
+        print u'(%s/%s) Downloading image %s...' % (i + 1, num_images,
+                                                    image.filename),
+        response = requests.get(image.original_url)
 
         if response.status_code == 200:
             # Download succeeded; let's save this thing!
-            with open(filename, 'wb') as image_file:
+            with open(image.filename, 'wb') as image_file:
                 image_file.write(response.content)
 
             print 'DONE'
@@ -56,8 +41,3 @@ def download_images(auth):
             # Oh noes!
             print 'FAILED'
 
-if __name__ == '__main__':
-    username = raw_input('imgur.com username: ')
-    password = getpass.getpass('imgur.com password: ')
-
-    download_images(auth = (username, password))
